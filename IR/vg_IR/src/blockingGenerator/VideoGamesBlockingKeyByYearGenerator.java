73d53470e9715bd890d47f12b8a2ea5f49668bba
katiefux <64419179+katiefux@users.noreply.github.com>
2020-11-29 21:19:10 +0100

    blocking by year
    is very inefficient

---
+++ IR/vg_IR/src/blockingGenerator/VideoGamesBlockingKeyByYearGenerator.java
@@ -0,0 +1,24 @@
+package blockingGenerator;
+
+
+import de.uni_mannheim.informatik.dws.winter.matching.blockers.generators.RecordBlockingKeyGenerator;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
+import de.uni_mannheim.informatik.dws.winter.model.Matchable;
+import de.uni_mannheim.informatik.dws.winter.model.Pair;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.processing.DataIterator;
+import de.uni_mannheim.informatik.dws.winter.processing.Processable;
+import genralClasses.VideoGames;
+
+public class VideoGamesBlockingKeyByYearGenerator extends
+RecordBlockingKeyGenerator<VideoGames, Attribute>{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public void generateBlockingKeys(VideoGames record, Processable<Correspondence<Attribute, Matchable>> correspondences,
+			DataIterator<Pair<String, VideoGames>> resultCollector) {
+		resultCollector.next(new Pair<>(Integer.toString(record.getDate()), record));
+	}
+
+}
