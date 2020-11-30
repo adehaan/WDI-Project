3ae32db6feeb964c434dabff7a8cec32c1f73697
adehaan <aniek.dhaan@gmail.com>
2020-11-30 20:33:33 +0100

    Added Rawg

--- IR/vg_IR/src/vG/Main.java
+++ IR/vg_IR/src/vG/Main.java
@@ -39,9 +39,9 @@ public class Main {
 
 	private static final Logger logger = WinterLogManager.activateLogger("trace");
 
-	public static void main(String[] args) throws Exception {
+	public static void wikitosales() throws Exception {
 		// TODO Auto-generated method stub
-		System.out.println("*\n*\tLoading datasets\n*");
+		System.out.println("*\n*\tLoading datasets wiki\n*");
 		HashedDataSet<VideoGames, Attribute> dataWiki = new HashedDataSet<>();
 		HashedDataSet<VideoGames, Attribute> dataSales = new HashedDataSet<>();
 		new GamesXMLReader().loadFromXML(new File("data/input/target_games_Wiki.xml"), "/Games/Game", dataWiki);
@@ -83,7 +83,7 @@ public class Main {
 		blocker.setMeasureBlockSizes(true);
 		
 		// Write debug results to file:
-		blocker.collectBlockSizeData("data/output/debugResultsBlocking.csv", 3300);
+		blocker.collectBlockSizeData("data/output/wiki_debugResultsBlocking.csv", 3300);
 
 		// Initialize Matching Engine
 		MatchingEngine<VideoGames, Attribute> engine = new MatchingEngine<>();
@@ -105,9 +105,100 @@ public class Main {
 		Performance perfTest = evaluator.evaluateMatching(correspondences, gsTest);
 
 		// print the evaluation result
-		System.out.println("Videogames Sale <-> Wiki");
+		System.out.println("Videogames Sales <-> Wiki");
 		System.out.println(String.format("Precision: %.4f", perfTest.getPrecision()));
 		System.out.println(String.format("Recall: %.4f", perfTest.getRecall()));
 		System.out.println(String.format("F1: %.4f", perfTest.getF1()));
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void rawgtosales() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("*\n*\tLoading datasets rawg\n*");
+		HashedDataSet<VideoGames, Attribute> dataRawg = new HashedDataSet<>();
+		HashedDataSet<VideoGames, Attribute> dataSales = new HashedDataSet<>();
+		
+		// TODO: Import all 11 files
+		new GamesXMLReader().loadFromXML(new File("data/input/rawg_target.xml"), "/Games/Game", dataRawg);
+		new GamesXMLReader().loadFromXML(new File("data/input/sales_target.xml"), "/Games/Game", dataSales);
+		
+		
+		// load the gold standard (test set)
+		System.out.println("*\n*\tLoading gold standard\n*");
+		MatchingGoldStandard gsTest = new MatchingGoldStandard();
+		gsTest.loadFromCSVFile(new File("../Goldstandard_rawg_wiki.csv"));
+
+		// create a matching rule
+		LinearCombinationMatchingRule<VideoGames, Attribute> matchingRule = new LinearCombinationMatchingRule<>(0.3);
+		matchingRule.activateDebugReport("data/output/rawg_debugResultsMatchingRule.csv", 1000000, gsTest);
+
+		//add comparators
+		matchingRule.addComparator(new GamesYearComparator2Years(), 0.4);
+		//matchingRule.addComparator(new GamesTitleComparatorEqual(), 0.7);
+		matchingRule.addComparator(new GamesTitleComparatorJaccard(), 0.4);
+		matchingRule.addComparator(new GamesPublisherComparatorLevenshtein(), 0.1);
+		//matchingRule.addComparator(new GamesTitleComparatorLevenshtein(), 0.7);
+		matchingRule.addComparator(new GamesPlatformComparatorLevenshtein(), 0.05);
+		matchingRule.addComparator(new GamesGenresComparatorLevenshtein(), 0.05);
+		//matchingRule.addComparator(new GamesGenresComparatorJaroWinkler(), 0.3);
+		//matchingRule.addComparator(new GamesPlatformComparatorJaroWinkler(), 0.3);
+	
+
+		//SortedNeighbourhoodBlocker<VideoGames, Attribute, Correspondence<Attribute,
+		//Matchable >> blocker = new SortedNeighbourhoodBlocker<VideoGames, Attribute, Correspondence<Attribute,
+		//		Matchable>>(new VideoGamesBlockingKeyByYearGenerator(),2);
+		// create a blocker (blocking strategy)
+		//SortedNeighbourhoodBlocker <VideoGames, Attribute, VideoGames> blocker = new SortedNeighbourhoodBlocker<VideoGames, Attribute, VideoGames>(new VideoGamesBlockingKeyByYearGenerator(),2);
+		
+		StandardRecordBlocker<VideoGames, Attribute> blocker = new StandardRecordBlocker<VideoGames, Attribute>(
+				new VideoGamesBlockingKeyByTitleYearGenerator());
+		
+		//NoBlocker blocker = new NoBlocker();
+		
+		blocker.setMeasureBlockSizes(true);
+		
+		// Write debug results to file:
+		blocker.collectBlockSizeData("data/output/rawg_debugResultsBlocking.csv", 3300);
+
+		// Initialize Matching Engine
+		MatchingEngine<VideoGames, Attribute> engine = new MatchingEngine<>();
+
+		// Execute the matching
+		System.out.println("*\n*\tRunning identity resolution\n*");
+		
+		Processable<Correspondence<VideoGames, Attribute>> correspondences = engine
+				.runIdentityResolution(dataSales, dataRawg, null, matchingRule, blocker);
+
+		// write the correspondences to the output file
+		
+		new CSVCorrespondenceFormatter().writeCSV(new File("data/output/Rawg_2_Sales_correspondences.csv"),
+				correspondences);
+
+		System.out.println("*\n*\tEvaluating result\n*");
+		// evaluate your result
+		MatchingEvaluator<VideoGames, Attribute> evaluator = new MatchingEvaluator<VideoGames, Attribute>();
+		Performance perfTest = evaluator.evaluateMatching(correspondences, gsTest);
+
+		// print the evaluation result
+		System.out.println("Videogames Sales <-> Rawg");
+		System.out.println(String.format("Precision: %.4f", perfTest.getPrecision()));
+		System.out.println(String.format("Recall: %.4f", perfTest.getRecall()));
+		System.out.println(String.format("F1: %.4f", perfTest.getF1()));	
+	}
+	
+	
+	// Run Both Matchings
+	public static void main(String[] args) throws Exception {
+		wikitosales();
+		rawgtosales();
+	}
+
+	
 }
