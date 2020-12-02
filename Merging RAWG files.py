e077c6a3a2f6e66f531fd53df87108f239805d27
JonasKlenk <jonas.klenk@gmail.com>
2020-12-02 22:44:22 +0100

    Extracting goldstandard from RAWG_xml_2.xml and adding them into RAWG_xml_1.zip

--- Merging RAWG files.py
+++ Merging RAWG files.py
@@ -4,6 +4,7 @@
   
 # Creating a list of filenames 
 import pathlib
+import os
 
 path= pathlib.Path(".\data\RAWG")
 flist = []
@@ -12,21 +13,31 @@ for p in pathlib.Path(path).iterdir():
         print(p)
         flist.append(p)
 
-filenames = ['file1.txt', 'file2.txt'] 
-  
-# Open file3 in write mode 
-with open('file3.txt', 'w') as outfile: 
-  
-    # Iterate through list 
-    for names in flist: 
-  
-        # Open each file in read mode 
-        with open(names) as infile: 
-  
-            # read the data from file1 and 
-            # file2 and write it in file3 
-            outfile.write(infile.read()) 
-  
-        # Add '\n' to enter data of file2 
-        # from next line 
-        outfile.write("\n") 
+
+
+c = 0
+while len(flist) != 0 :
+    setof11 = [] 
+    while len(setof11) < 11 and len(flist) != 0:
+        setof11.append(str(flist.pop(0)))
+    c += 1
+
+
+    # Open file3 in write mode 
+    with open(".\data\RAWG_concat\cfile" + str(c) +'.jsonl', 'w') as outfile: 
+    
+        # Iterate through list 
+        for names in setof11: 
+    
+            # Open each file in read mode 
+            with open(names) as infile: 
+    
+                # read the data from file1 and 
+                # file2 and write it in file3 
+                outfile.write(infile.read()) 
+    
+            # Add '\n' to enter data of file2 
+            # from next line 
+            outfile.write("\n") 
+
+print("Fertig")
